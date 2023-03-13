7298093184cbef0e3f1ad7f77ef9ab76d3b56f3f
loudpose <chrisaveryano@gmail.com>
2023-03-13 16:19:30 +0200

    lenis-scroll-test

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,14 @@
 import Btn from './components/Button';
 import Preloader from './components/Preloader';
-
+import Lenis from '@studio-freight/lenis';
 import coverHome from './pages/home';
 
 class App {
 	constructor() {
 		this.create();
 		this.createPreloader();
+		this.createScroll();
+		this.update();
 	}
 
 	create() {
@@ -19,8 +21,23 @@ class App {
 		this.preloader.once('completed', this.onPreloaded.bind(this));
 	}
 
+	createScroll() {
+		this.scroll = new Lenis({
+			duration: 1,
+			easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+		});
+
+		// this.scroll.on('scroll', ScrollTrigger.update);
+	}
+
 	onPreloaded() {
 		this.preloader.hide();
+	}
+
+	update(e) {
+		this.frame = window.requestAnimationFrame(this.update.bind(this));
+
+		this.scroll.raf(e);
 	}
 }
 
